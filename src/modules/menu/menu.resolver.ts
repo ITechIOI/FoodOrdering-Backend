d4d0f4a3dafad087574d57515ce37c2c759ece56
ITechIOI <22520783@gm.uit.edu.vn>
2025-05-01 17:41:47 +0700

    fix(API): Update name of searchNearestItemByKeyword API

--- src/modules/menu/menu.resolver.ts
+++ src/modules/menu/menu.resolver.ts
@@ -61,7 +61,7 @@ export class MenuResolver {
   }
 
   @Query(() => [NearbyMenuItem])
-  async searchNearbyMenuItems(
+  async searchNearestItemByKeyword(
     @Args('latitude', { type: () => Float }) latitude: number,
     @Args('longitude', { type: () => Float }) longitude: number,
     @Args('keyword') keyword: string,
@@ -73,7 +73,7 @@ export class MenuResolver {
       keyword,
       limit,
     );
-  }
+  } 
 
   @Query(() => [NearbyMenuItem])
   async searchNearestMenuItems(
